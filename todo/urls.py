ea41c65d9ef32008dc9790eabd93376221abb3e2
ntth2910 <haxinguyen210999@gmail.com>
2025-08-05 21:43:48 +0700

    crud

--- todo/urls.py
+++ todo/urls.py
@@ -7,5 +7,8 @@ from todo import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
-
+    path('markTaskAsDone/<int:id>/', views.markTaskAsDone, name='markTaskAsDone'),
+    path('markTaskAsUnDone/<int:id>/', views.markTaskAsUnDone, name='markTaskAsUnDone'),
+    path('deleteTask/<int:id>/', views.deleteTask, name='deleteTask'),
+    path('editTask/<int:id>/', views.editTask, name='editTask'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from todo.models import Task
 
@@ -17,3 +17,40 @@ def addTask(request):
     # tas = request.POST['task']  # This will raise an error if 'task' is not in POST data
 
     return redirect('home')
+
+def markTaskAsDone(request, id):
+    # Logic to mark a task as done
+    task = get_object_or_404(Task, id=id)
+    task.is_completed = True
+    task.save()
+
+    return redirect('home')
+
+def markTaskAsUnDone(request, id):
+    # Logic to mark a task as undone
+    task = get_object_or_404(Task, id=id)
+    task.is_completed = False
+    task.save()
+
+    return redirect('home')
+
+def deleteTask(request, id):
+    # Logic to delete a task
+    task = get_object_or_404(Task, id=id)
+    task.delete()
+
+    return redirect('home')
+def editTask(request, id):
+    # Logic to edit a task
+    task = get_object_or_404(Task, id=id)
+
+    if request.method == 'POST':
+       newTask = request.POST['task']
+       task.task = newTask
+       task.save()
+       return redirect('home')
+    else :
+        context = {
+            'task': task,
+        }
+        return render(request, 'edit_task.html', context)
